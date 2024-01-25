4f7a5550083c3e2a64ea48828ce3f3dc40148472
Tobias Rick <turboschnecke2016@gmail.com>
2024-01-25 09:14:39 +0100

    Update main.java
    Enhancements of the main function to make user comfort better. And split up in different functions

--- UserLogin/src/main.java
+++ UserLogin/src/main.java
@@ -1,40 +1,38 @@
 import java.util.Scanner;
 
-public class main {
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your username: ");
+        System.out.print("Enter your username: ");
         String username = scanner.nextLine();
-        System.out.println("Password: ");
+        System.out.print("Password: ");
         String password = scanner.nextLine();
 
-        if(username.equals("patika"))
-        {
-            if(password.equals("java123"))
-            {
+        if (isValidLogin(username, password)) {
             System.out.println("You are logged in.");
-            }else
-            {
-                System.out.println("Your password is wrong... Want to restart your password ? 1/0 ");
-                int answer = scanner.nextInt();
-                if (answer == 1)
-                {
-                    System.out.println("Enter your new password: ");
-                    scanner.nextLine();
-                    String newPass = scanner.nextLine();
-                    if(newPass.equals("java123"))
-                    {
-                        System.out.println("New password cannot be the same as old one, please enter a new one.");
-                    }else
-                    {
-                        System.out.println("Your password updated.");
-                    }
-                }
-            }
-        }else {
+        } else {
             System.out.println("Your username or password is wrong.");
+            System.out.print("Want to reset your password? (1 for yes, 0 for no): ");
+            int answer = scanner.nextInt();
+            if (answer == 1) {
+                resetPassword(scanner);
+            }
+        }
+    }
+
+    private static boolean isValidLogin(String username, String password) {
+        return username.equals("patika") && password.equals("java123");
+    }
+
+    private static void resetPassword(Scanner scanner) {
+        System.out.print("Enter your new password: ");
+        String newPass = scanner.next();
+        if (newPass.equals("java123")) {
+            System.out.println("New password cannot be the same as the old one, please enter a new one.");
+            resetPassword(scanner);
+        } else {
+            System.out.println("Your password is updated.");
         }
     }
 }
